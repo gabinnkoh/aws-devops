2723465e0240626387d4dd9e4196c49bb3779fcc
gabinnkoh <boriegabin@gmail.com>
2022-11-18 17:05:13 +0900

    commit

--- src/main/java/net/cj/edu/aws/MainController.java
+++ src/main/java/net/cj/edu/aws/MainController.java
@@ -1,5 +1,11 @@
 package net.cj.edu.aws;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,31 @@ public class MainController {
 	@GetMapping("/") //@requestMapping(value="/", method=requestmethod.GET)이랑 똑같음
 	public String home() {
 		return "<h1>aws-devops</h1>"; //string을 넘기는 것이다
+	}
+	
+	@GetMapping("/date")
+	public String date() throws ClassNotFoundException {
+		Class.forName("org.postgresql.Driver");
+
+		String connurl = "jdbc:postgresql://aws-devops-db2.cniixrjqazcz.ap-northeast-2.rds.amazonaws.com:5432/postgres";
+		String user = "postgres";
+		String password = "12345678";
+		String result = "NO_DATA";
+
+		try (Connection connection = DriverManager.getConnection(connurl, user, password);) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT now() AS date");
+
+			while (rs.next()) {
+				result = rs.getString("date");
+			}
+			rs.close();
+			stmt.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
 	}
 }
